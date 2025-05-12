0763961e63342424149dbb0cee3768747a99749d
Malik Piara <malik@hey.com>
2025-05-12 18:15:30 +0100

    Implemented 'copy to clipboard' feature

--- apps/web/components/shareWidget.tsx
+++ apps/web/components/shareWidget.tsx
@@ -1,9 +1,27 @@
 "use client";
 import { CatIcon, Link } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "./tailwind/ui/button";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./tailwind/ui/hover-card";
 
 export default function ShareWidget() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  // Function to copy the current URL to clipboard
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      setIsCopied(true);
+
+      toast("URL Copied!");
+
+      // Reset the copied state after 2 seconds
+      setTimeout(() => setIsCopied(false), 2000);
+    } catch (error) {
+      toast("Failed to copy");
+    }
+  };
   return (
     <>
       <HoverCard>
@@ -15,7 +33,7 @@ export default function ShareWidget() {
         <HoverCardContent className="w-48 p-0" sideOffset={10} align="start">
           <div className="flex gap-6 m-0">
             <div>
-              <Button variant="ghost" className="w-full justify-start flex gap-2">
+              <Button variant="ghost" className="w-full justify-start flex gap-2" onClick={copyToClipboard}>
                 <Link className="mr-2 h-4 w-4 " />
                 Copy URL
               </Button>
