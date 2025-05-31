4eb33e75d1df6aae949d6438e0e906efdebcf0b1
Malik Piara <malik@hey.com>
2025-05-31 02:46:42 +0100

    Minor styling changes.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <div>
               <Navbar />
               <main className="max-w-screen-lg mx-auto p-4">{children}</main>
-              <div className="justify-start tracking-wider uppercase [writing-mode:vertical-rl] left-0 bottom-0 hidden sm:block fixed text-xs m-2 text-muted-foreground hover:blur-sm transition-all duration-1000 hover:bg-gradient-to-r hover:from-[#24acb5] hover:to-[#2cdce9] bg-clip-text hover:text-transparent cursor-pointer">
+              <div className="justify-start tracking-wider uppercase [writing-mode:vertical-rl] left-0 bottom-1 hidden sm:block fixed text-xs m-2 text-muted-foreground hover:blur-sm transition-all duration-1000 hover:bg-gradient-to-r hover:from-[#24acb5] hover:to-[#2cdce9] bg-clip-text hover:text-transparent cursor-pointer">
                 Only you know who you can be
               </div>
               <footer className="justify-end flex gap-3 text-muted-foreground p-4 mb-2 border-t">
