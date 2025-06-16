c9e446e62dce90b2e98620bf23d8d42d6eea1026
Malik Piara <malik@hey.com>
2025-06-16 14:06:15 +0100

    Fixed another TS issue in add event page.

--- apps/web/app/add-event/layout.tsx
+++ apps/web/app/add-event/layout.tsx
@@ -1,3 +1,5 @@
-export default function Layout({ children }) {
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
   return <main className="w-full absolute left-0">{children}</main>;
 }
